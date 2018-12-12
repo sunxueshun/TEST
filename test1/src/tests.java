2431acfc38053557908524afb8d2584cbbb46152
sunxueshun <1065082109@qq.com>
2018-12-12 20:29:16 +0800

    1.01

--- test1/src/tests.java
+++ test1/src/tests.java
@@ -1,5 +1,13 @@
 public class tests {
     public static void main(String[] args) {
         System.out.println("sds");
+        System.out.println("123");
+        Iteam iteam = new Iteam();
+        iteam.setId(2);
+        iteam.setName("水电费");
+        System.out.println(iteam.toString());
+
+
+
     }
 }
